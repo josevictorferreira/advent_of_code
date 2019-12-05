801bf8607c8c2a42473e181854c5eeef3e2fa930
José Victor Ferreira <josevictorferreira@outlook.com>
2019-12-05 11:35:04 -0300

    Added fourth day part 2

--- lib/2019/day4/part_2.exs
+++ lib/2019/day4/part_2.exs
@@ -8,21 +8,14 @@ input =
 defmodule CheckPasswords do
   def check_range([init | [final | []]]) do
     Enum.to_list(init..final)
-
-    [111_122]
     |> Enum.map(&Integer.to_string/1)
     |> Enum.filter(fn x ->
-      IO.inspect(String.match?(x, ~r/(?=^((\d)\2?(?!\2))+$)(?=^1*2*3*4*5*6*7*8*9*$)/))
-      IO.inspect(String.match?(x, ~r/(?=.*(\d)\1{2,}.*)(?=^1*2*3*4*5*6*7*8*9*$)/))
-
-      String.match?(x, ~r/(?=^((\d)\2?(?!\2))+$)(?=^1*2*3*4*5*6*7*8*9*$)/) ||
-        (String.match?(x, ~r/(?=^((\d)\2?(?!\2))+$)(?=^1*2*3*4*5*6*7*8*9*$)/) &&
-           String.match?(x, ~r/(?=.*(\d)\1{2,}.*)(?=^1*2*3*4*5*6*7*8*9*$)/))
+      String.match?(x, ~r/(?=^1*2*3*4*5*6*7*8*9*$)/) &&
+        String.match?(x, ~r/(?=((^)|(.))((?(3)(?!\1).|.))\4(?!\4))/)
     end)
-    |> IO.inspect()
     |> length()
   end
 end
 
 CheckPasswords.check_range(input)
-|> IO.inspect()
+|> IO.puts()
